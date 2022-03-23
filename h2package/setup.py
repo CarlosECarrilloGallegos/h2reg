c4c42902adcc63e9ae4db12949ab3a1fc9d21fcb
Carlos Carrillo-Gallegos <90050144+CarlosECarrilloGallegos@users.noreply.github.com>
2022-03-23 00:18:25 -0400

    Add files via upload

---
+++ h2package/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="h2reg",
+    version="0.1",
+    author="Carlos Carrillo-Gallegos",
+    author_email="carlos.carrillo-gallegos@yale.edu",
+    description="Kinematics of HII Regions",
+    packages=["h2reg","h2reg/Kinematics","h2reg/Mass","h2reg/ColumnDensity"]
+
+)
